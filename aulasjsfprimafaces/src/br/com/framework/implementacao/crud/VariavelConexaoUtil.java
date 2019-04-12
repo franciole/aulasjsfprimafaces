eb0a704710bc9ae852a70de6800204076c9c8396
Franciole Bezerra <franciole.bezerra@gmail.com>
2019-04-12 03:02:35 -0300

    Comite inicial do Eclipe

---
+++ aulasjsfprimafaces/src/br/com/framework/implementacao/crud/VariavelConexaoUtil.java
@@ -0,0 +1,13 @@
+package br.com.framework.implementacao.crud;
+
+/**
+ * Nome do cmainho do JNDI datasource Tomcat
+ * 
+ * @author FLB
+ *
+ */
+
+public class VariavelConexaoUtil {
+
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
+}
